45c5b389da1d37a218718cec6dd6cc3a06ee9e1b
Naoki Arakawa <naoki.arakawa1026@gmail.com>
2021-06-13 00:26:16 +0900

    3-4 レイアウトデータのバインディングまで

--- app/src/main/java/com/example/jetpackdata/MainActivity.kt
+++ app/src/main/java/com/example/jetpackdata/MainActivity.kt
@@ -21,47 +21,13 @@ class MainActivity : AppCompatActivity() {
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val myViewModel = ViewModelProvider(this).get(MyViewModel::class.java)
-
-        // Name
-        val nameObserver = Observer<String> { newName ->
-            binding.frmName.setText(newName, TextView.BufferType.NORMAL)
-        }
-        myViewModel.name.observe(this, nameObserver)
-
-        // Mail
-        val mailObserver = Observer<String> { newMail ->
-            binding.frmEmail.setText(newMail, TextView.BufferType.NORMAL)
-        }
-        myViewModel.mail.observe(this, mailObserver)
-
-        // Age
-        val ageObserver = Observer<String> { newAge ->
-            binding.frmAge.setText(newAge, TextView.BufferType.NORMAL)
-        }
-        myViewModel.age.observe(this, ageObserver)
-
-        // All
-        val allObserver = Observer<String> { newAll ->
-            binding.text1.setText(newAll, TextView.BufferType.NORMAL)
-        }
-        myViewModel.allText.observe(this, allObserver)
-
+        binding.vName = ""
+        binding.vPass = ""
 
         binding.button1.setOnClickListener {
-            myViewModel.add(
-                binding.frmName.text.toString(),
-                binding.frmEmail.text.toString(),
-                binding.frmAge.text.toString().toInt()
-            )
-
-            myViewModel.name.value = ""
-            myViewModel.mail.value = ""
-            myViewModel.age.value = "0"
-            myViewModel.allText.value = myViewModel.allByText()
+            binding.vName = binding.frmName.text.toString()
+            binding.vPass = binding.frmPass.text.toString()
         }
-
-
 
     }
 }
