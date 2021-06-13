bae9d798307262c52b7651b5818534fef0798a4f
Naoki Arakawa <naoki.arakawa1026@gmail.com>
2021-06-13 11:40:57 +0900

    3-4 レイアウトファイルを用いたダイアログの表示

--- app/src/main/java/com/example/jetpackdata/MainActivity.kt
+++ app/src/main/java/com/example/jetpackdata/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.jetpackdata
 import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.ViewModelProvider
@@ -24,11 +25,20 @@ class MainActivity : AppCompatActivity() {
 
         binding.text1.text = myViewModel.allByText()
         binding.button1.setOnClickListener {
+
+            val vw = layoutInflater.inflate(R.layout.mydialog, null)
+            val dlg_name = vw.findViewById<EditText>(R.id.dlg_name)
+            val dlg_mail = vw.findViewById<EditText>(R.id.dlg_mail)
+            val dlg_age = vw.findViewById<EditText>(R.id.dlg_age)
+
+
             val builder = AlertDialog.Builder(this)
             builder.setTitle("Alert")
+                builder.setView(vw)
                 .setMessage("Add new persond data?")
                 .setPositiveButton("OK", DialogInterface.OnClickListener{ dialog, id ->
-                    myViewModel.add("new name", "new@mail.address", 123)
+                    myViewModel.add(dlg_name.text.toString(), dlg_mail.text.toString(), dlg_age.text.toString().toInt())
+
                     binding.text1.text = myViewModel.allByText()
                 })
                 .setNegativeButton("Cancel", DialogInterface.OnClickListener{ dialog, id ->
